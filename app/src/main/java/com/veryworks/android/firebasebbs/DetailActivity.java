7c2ba2e76aaf6b3417cd4fb998032677266f252a
javafa <nigne2016@gmail.com>
2017-07-04 11:32:49 +0900

    add firebase storage

--- app/src/main/java/com/veryworks/android/firebasebbs/DetailActivity.java
+++ app/src/main/java/com/veryworks/android/firebasebbs/DetailActivity.java
@@ -1,30 +1,79 @@
 package com.veryworks.android.firebasebbs;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.veryworks.android.firebasebbs.domain.Bbs;
+
+import java.io.File;
 
 public class DetailActivity extends AppCompatActivity {
     EditText editTitle, editAuthor, editContent;
+    TextView textImage;
+    // 데이터베이스
     FirebaseDatabase database;
     DatabaseReference bbsRef;
+    // 스토리지
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        // 데이터베이스 레퍼런스
         database = FirebaseDatabase.getInstance();
         bbsRef = database.getReference("bbs");
 
+        // 스토리지 레퍼런스
+        mStorageRef = FirebaseStorage.getInstance().getReference("images");
+
         setContentView(R.layout.activity_detail);
+
         editTitle = (EditText) findViewById(R.id.editTitle);
         editAuthor = (EditText) findViewById(R.id.editAuthor);
         editContent = (EditText) findViewById(R.id.editContent);
+        textImage = (TextView) findViewById(R.id.textImage);
+    }
+
+    public void uploadFile(String filePath){
+        // 스마트폰에 있는 파일의 경로
+        File file = new File(filePath);
+        Uri uri = Uri.fromFile(file);
+        // 파이어베이스에 있는 파일 경로
+        String fileName = file.getName(); // + 시간값 or UUID 추가해서 만듬
+        // 데이터베이스의 키 = 값과 동일한 구조
+        StorageReference fileRef = mStorageRef.child(fileName);
+
+        fileRef.putFile(uri)
+            .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // Get a URL to the uploaded content
+                    // Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    Log.i("FBStorage","Upload Success!");
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                    Log.e("FBStorage","Upload Fail:"+exception.getMessage());
+                }
+            });
     }
 
     // 데이터 전송
@@ -45,5 +94,26 @@ public class DetailActivity extends AppCompatActivity {
         //    delete : bbsRef.child(bbsKey).setValue(null);
         // 데이터 입력후 창 닫기
         finish();
+    }
+
+    // 화면의 Gallery 버튼에서 자동 링크
+    public void openGallery(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // 가. 이미지 선택창 호출
+        startActivityForResult( Intent.createChooser(intent, "앱을 선택하세요") , 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            switch (requestCode) {
+                // 나. 이미지 선택창에서 선택된 이미지의 경로 추출
+                case 100:
+                    Uri imageUri = data.getData();
+                    textImage.setText(imageUri.getPath());
+                    break;
+            }
+        }
     }
 }
--- app/src/main/java/com/veryworks/android/firebasebbs/MainActivity.java
+++ app/src/main/java/com/veryworks/android/firebasebbs/MainActivity.java
@@ -2,19 +2,39 @@ package com.veryworks.android.firebasebbs;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.veryworks.android.firebasebbs.util.PermissionControl;
+
+public class MainActivity extends AppCompatActivity implements PermissionControl.CallBack{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PermissionControl.checkVersion(this);
     }
 
     public void startBbs(View view){
         Intent intent = new Intent(this, ListActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PermissionControl.onResult(this, requestCode, grantResults);
+    }
+
+    @Override
+    public void init() {
+        Intent intent = new Intent(this, NaviActivity.class);
+        startActivity(intent);
+
+        // 스플래쉬 화면은 backstack에 남아있으면 안되므로 종료
+        finish();
     }
 }
